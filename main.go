6d45e7175253dd0d00391843b4d3a47f1fb47bc0
alvinw <revenger.vinz90@gmail.com>
2023-05-08 16:27:21 +0700

    add schedule check

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/joho/godotenv"
@@ -34,9 +35,25 @@ func main() {
 		"untuk umur yang lebih panjang+\n" +
 		"🙏😌\n"
 
-	msg := tgbotapi.NewMessage(chatID, content)
-	_, err = bot.Send(msg)
-	if err != nil {
-		log.Fatal(err)
+	for {
+		now := time.Now()
+		targetTime1 := time.Date(now.Year(), now.Month(), now.Day(), 10, 0, 0, 0, now.Location())
+		targetTime2 := time.Date(now.Year(), now.Month(), now.Day(), 13, 0, 0, 0, now.Location())
+		targetTime3 := time.Date(now.Year(), now.Month(), now.Day(), 16, 0, 0, 0, now.Location())
+		targetTime4 := time.Date(now.Year(), now.Month(), now.Day(), 17, 0, 0, 0, now.Location())
+		targetTime5 := time.Date(now.Year(), now.Month(), now.Day(), 18, 0, 0, 0, now.Location())
+
+		if now.After(targetTime1) && now.Before(targetTime1.Add(time.Minute)) ||
+			now.After(targetTime2) && now.Before(targetTime2.Add(time.Minute)) ||
+			now.After(targetTime3) && now.Before(targetTime3.Add(time.Minute)) ||
+			now.After(targetTime4) && now.Before(targetTime4.Add(time.Minute)) ||
+			now.After(targetTime5) && now.Before(targetTime5.Add(time.Minute)) {
+
+			msg := tgbotapi.NewMessage(chatID, content)
+			_, err = bot.Send(msg)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
 	}
 }
